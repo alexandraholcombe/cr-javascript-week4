59a8592ced633908e2171d9483fabe6ce71e483c
Alexandra Holcombe <alexandra.holcombe@gmail.com>
2017-04-07 15:19:33 -0700

    Add list of musicians to admin page

--- src/app/admin/admin.component.ts
+++ src/app/admin/admin.component.ts
@@ -1,16 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+import { Musician } from '../musician.model';
+import { MusiciansService } from '../musicians.service';
+import { AngularFire, FirebaseListObservable } from 'angularfire2';
 
 @Component({
   selector: 'app-admin',
   templateUrl: './admin.component.html',
-  styleUrls: ['./admin.component.scss']
+  styleUrls: ['./admin.component.scss'],
+  providers: [MusiciansService]
 })
 export class AdminComponent implements OnInit {
+  allMusicians: FirebaseListObservable<any[]>;
   showAddForm: boolean = false;
 
-  constructor() { }
+  constructor(private musiciansService: MusiciansService) { }
 
   ngOnInit() {
+    this.allMusicians = this.musiciansService.getMusicians();
   }
 
   clickShow() {
