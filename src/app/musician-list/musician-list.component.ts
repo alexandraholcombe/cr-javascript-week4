bbeb7a13df7cc3a03dd59e4ac570c7c9f603faee
Alexandra Holcombe <alexandra.holcombe@gmail.com>
2017-04-09 15:11:42 -0700

    Add instrument filter selectbox

--- src/app/musician-list/musician-list.component.ts
+++ src/app/musician-list/musician-list.component.ts
@@ -1,8 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { Musician } from '../musician.model';
 import { Section } from '../section.model';
+import { Instrument } from '../instrument.model';
 import { MusiciansService } from '../musicians.service';
 import { SectionsService } from '../sections.service';
+import { InstrumentsService } from '../instruments.service';
 import { AngularFire, FirebaseListObservable } from 'angularfire2';
 import { Router } from '@angular/router';
 
@@ -10,24 +12,30 @@ import { Router } from '@angular/router';
   selector: 'app-musician-list',
   templateUrl: './musician-list.component.html',
   styleUrls: ['./musician-list.component.scss'],
-  providers: [MusiciansService, SectionsService]
+  providers: [MusiciansService,
+              SectionsService,
+              InstrumentsService]
 })
 
 export class MusicianListComponent implements OnInit {
   public allMusicians: FirebaseListObservable<any[]>;
-  public currentRoute: string = this.router.url;
-  public filterBySection: string = 'allMusicians';
+  public currentRoute = this.router.url;
+  public filterBySection = 'allMusicians';
+  public filterByInstrument = 'allMusicians';
   public allSections: Section[];
+  public allInstruments: Instrument[];
 
   constructor(
     private musiciansService: MusiciansService,
     private sectionsService: SectionsService,
+    private instrumentsService: InstrumentsService,
     private router: Router
   ) { }
 
   ngOnInit() {
     this.allMusicians = this.musiciansService.getMusicians();
     this.allSections = this.sectionsService.getSections();
+    this.allInstruments = this.instrumentsService.getInstruments();
   }
 
   setDisplay() {
@@ -52,5 +60,9 @@ export class MusicianListComponent implements OnInit {
 
   onSectionChange(optionFromMenu) {
     this.filterBySection = optionFromMenu;
+  }
+
+  onInstrumentChange(optionFromMenu) {
+    this.filterByInstrument = optionFromMenu;
   }
 }
