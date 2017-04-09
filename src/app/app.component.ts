3de5c64555aa0a1bfed3609ebbaafc7b8e33d665
Alexandra Holcombe <alexandra.holcombe@gmail.com>
2017-04-08 19:54:58 -0700

    Style homepage

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  public currentRoute: string = this.router.url;
+
+
+  constructor(
+    private router: Router
+  ) { }
+
+  ngOnInit() {
+    console.log(this.currentRoute);
+  }
+
   title = 'app works!';
 }
