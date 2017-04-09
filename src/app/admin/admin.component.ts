2490b3916c1e2ea7cad5739e4a18177aa721f8fd
Alexandra Holcombe <alexandra.holcombe@gmail.com>
2017-04-09 15:59:40 -0700

    Add filtering to admin page

--- src/app/admin/admin.component.ts
+++ src/app/admin/admin.component.ts
@@ -1,22 +1,38 @@
 import { Component, OnInit } from '@angular/core';
 import { Musician } from '../musician.model';
+import { Section } from '../section.model';
+import { Instrument } from '../instrument.model';
 import { MusiciansService } from '../musicians.service';
+import { SectionsService } from '../sections.service';
+import { InstrumentsService } from '../instruments.service';
 import { AngularFire, FirebaseListObservable } from 'angularfire2';
 
 @Component({
   selector: 'app-admin',
   templateUrl: './admin.component.html',
   styleUrls: ['./admin.component.scss'],
-  providers: [MusiciansService]
+  providers: [MusiciansService,
+              SectionsService,
+              InstrumentsService]
 })
 export class AdminComponent implements OnInit {
-  allMusicians: FirebaseListObservable<any[]>;
-  showAddForm: boolean = false;
+  public allMusicians: FirebaseListObservable<any[]>;
+  public filterBySection = 'allSections';
+  public filterByInstrument = 'allInstruments';
+  public allSections: Section[];
+  public allInstruments: Instrument[];
+  public showAddForm = false;
 
-  constructor(private musiciansService: MusiciansService) { }
+  constructor(
+    private musiciansService: MusiciansService,
+    private sectionsService: SectionsService,
+    private instrumentsService: InstrumentsService
+  ) { }
 
   ngOnInit() {
     this.allMusicians = this.musiciansService.getMusicians();
+    this.allSections = this.sectionsService.getSections();
+    this.allInstruments = this.instrumentsService.getInstruments();
   }
 
   clickShow() {
@@ -25,6 +41,14 @@ export class AdminComponent implements OnInit {
 
   resetForm() {
     this.showAddForm = false;
+  }
+
+  onSectionChange(optionFromMenu) {
+    this.filterBySection = optionFromMenu;
+  }
+
+  onInstrumentChange(optionFromMenu) {
+    this.filterByInstrument = optionFromMenu;
   }
 
 }
